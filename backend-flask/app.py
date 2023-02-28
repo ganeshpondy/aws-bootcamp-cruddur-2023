5e9ab325fb88402eacfa7a05ace7a59f969efb9f
ganeshpondy <ganeshpondy@gmail.com>
2023-02-28 01:28:13 +0000

    Configure CloudWatch

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,6 +26,10 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# CloudWatch
+import watchtower
+import logging
+from time import strftime
 
 # HoneyComb
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -38,6 +42,16 @@ tracer = trace.get_tracer(__name__)
 # X-RAY
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+# CloudWatch
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("test log")
 
 
 app = Flask(__name__)
@@ -60,6 +74,13 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+# CloudWatch
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta, timezone
 class HomeActivities:
   def run():
+    # logger.info("HomeActivities")
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
