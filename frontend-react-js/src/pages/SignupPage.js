a808be90aa8c7ec4c40bf9e7f4f05a6d3532cadb
ganeshpondy <ganeshpondy@gmail.com>
2023-03-09 15:06:16 +0000

    Cognito-FrontEnd-Config-Done

--- frontend-react-js/src/pages/SignupPage.js
+++ frontend-react-js/src/pages/SignupPage.js
@@ -3,8 +3,10 @@ import React from "react";
 import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 
+// Week-03 Cognito
+import { Auth } from 'aws-amplify';
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 
 export default function SignupPage() {
 
@@ -17,16 +19,41 @@ export default function SignupPage() {
 
   const onsubmit = async (event) => {
     event.preventDefault();
-    console.log('SignupPage.onsubmit')
-    // [TODO] Authenication
-    Cookies.set('user.name', name)
-    Cookies.set('user.username', username)
-    Cookies.set('user.email', email)
-    Cookies.set('user.password', password)
-    Cookies.set('user.confirmation_code',1234)
-    window.location.href = `/confirm?email=${email}`
+    setErrors('')
+    try {
+        const { user } = await Auth.signUp({
+          username: email,
+          password: password,
+          attributes: {
+            name: name,
+            email: email,
+            preferred_username: username,
+          },
+          autoSignIn: { // optional - enables auto sign in after user is confirmed
+            enabled: true,
+          }
+        });
+        console.log(user);
+        window.location.href = `/confirm?email=${email}`
+    } catch (error) {
+        console.log(error);
+        setErrors(error.message)
+    }
     return false
   }
+
+  // const onsubmit = async (event) => {
+  //   event.preventDefault();
+  //   console.log('SignupPage.onsubmit')
+  //   // [TODO] Authenication
+  //   Cookies.set('user.name', name)
+  //   Cookies.set('user.username', username)
+  //   Cookies.set('user.email', email)
+  //   Cookies.set('user.password', password)
+  //   Cookies.set('user.confirmation_code',1234)
+  //   window.location.href = `/confirm?email=${email}`
+  //   return false
+  // }
 
   const name_onchange = (event) => {
     setName(event.target.value);
