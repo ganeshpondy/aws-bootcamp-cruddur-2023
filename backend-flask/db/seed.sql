479e2a0cca7cb4afc593d3cd7231a3c727a5c3aa
ganeshpondy <ganeshpondy@gmail.com>
2023-03-12 13:32:50 +0000

    Week-03-Postgres-InProgress

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,8 @@
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
   ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Andrew Bayko', 'bayko' ,'MOCK'),
+  ('Ganesh B', 'ganeshpondy' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
