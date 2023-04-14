ed309452d4ab2fb781c2492c147a248fa0a80af7
ganeshpondy <ganeshpondy@gmail.com>
2023-04-14 02:22:49 +0000

    cdk synth error

--- thumbing-serverless-cdk/test/thumbing-serverless-cdk.test.ts
+++ thumbing-serverless-cdk/test/thumbing-serverless-cdk.test.ts
@@ -5,13 +5,13 @@
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/thumbing-serverless-cdk-stack.ts
 test('SQS Queue Created', () => {
-    //   const app = new cdk.App();
-    //     // WHEN
-    //   const stack = new ThumbingServerlessCdk.ThumbingServerlessCdkStack(app, 'MyTestStack');
-    //     // THEN
-    //   const template = Template.fromStack(stack);
-    
-    //   template.hasResourceProperties('AWS::SQS::Queue', {
-    //     VisibilityTimeout: 300
-    //   });
-    });
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ThumbingServerlessCdk.ThumbingServerlessCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
